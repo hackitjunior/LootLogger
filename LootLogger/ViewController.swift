fc8d150db8c1cdc480cb7aa086d271b550b3507c
Alberto Silva <silvaguzmanalberto@gmail.com>
2021-05-28 12:54:08 -0500

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Alberto Silva on 28/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
