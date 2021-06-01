f49d25bd4341c9c430f9181a61ffb72e5c18c8da
Alberto Silva <silvaguzmanalberto@gmail.com>
2021-06-01 11:48:05 -0500

    Display the items with custom view and dynamic text

---
+++ LootLogger/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Alberto Silva on 01/06/21.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
