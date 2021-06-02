08936df1d83d4c7a5ad1dddae4fc69312c495b25
Alberto Silva <silvaguzmanalberto@gmail.com>
2021-06-02 12:34:47 -0500

    Display the details of a item

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -79,3 +79,18 @@ extension ItemsViewController {
         }
     }
 }
+
+extension ItemsViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showItem":
+            if let row = tableView.indexPathForSelectedRow?.row {
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.item = item
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
+    }
+}
